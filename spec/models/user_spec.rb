bb6582166d0f5cf8eceac4bb9ba054e3a0f37cf5
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-23 19:17:12 -0500

    update api key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'relationships' do
+    it { should validate_presence_of(:email)}
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password)}
     it { should have_many :user_viewing_parties }
     it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
 
   describe '#user_host_name' do
     it 'can return the name of a user given their id' do
-      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com')
+      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com', password: '12345')
 
       query = User.user_host_name(user_1.id)
 
@@ -20,9 +23,9 @@ RSpec.describe User, type: :model do
 
   describe '#all_users_except_self' do
     it 'can return a list of all users excluding user argument' do
-      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com')
-      user_2 = User.create!(name: 'Katie', email: '34997@gmail.com')
-      user_3 = User.create!(name: 'George', email: 'george@gmail.com')
+      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com', password: '12345')
+      user_2 = User.create!(name: 'Katie', email: '34997@gmail.com', password: '12345')
+      user_3 = User.create!(name: 'George', email: 'george@gmail.com', password: '12345')
 
       query = User.all_users_except_self(user_1)
 
